6fe91794ccc611cd3d73e2195c7cc1e2ada6300a
santomi3112 <adiprasetio007@gmail.com>
2023-02-02 13:27:38 +0700

    added readline feature

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const axios = require('axios')
-const email = 'busetgan@gmail.com'
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+readline.question('enter your email: ', (email) => {
+  axios.get(`https://twitter.com/users/email_available?email=${email}`,
+  )
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  readline.close()
+})
 
-axios.get(`https://twitter.com/users/email_available?email=${email}`,
-)
-  .then(res => {
-    console.log(res.data);
-  })
-  .catch(err => {
-    console.error(err)
-  })
